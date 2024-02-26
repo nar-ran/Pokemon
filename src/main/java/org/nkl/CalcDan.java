9035e14a354d46ff6bbd2ec59a5b3fa1e4deae79
N R <143750294+nar-ran@users.noreply.github.com>
2024-02-25 19:18:03 -0500

    Proyecto con cuestiones de validaciones.

--- src/main/java/org/nkl/CalcDan.java
+++ src/main/java/org/nkl/CalcDan.java
@@ -1,34 +1,64 @@
 package org.nkl;
 
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CalcDan {
-    List<> efectividad = new ArrayList<>();
+public class CalcDan{
+    double danio, efectividad;
+    public void cal(String pokemonAtaq, double atq, String pokemonDef, double def){
+        if(pokemonDef.equals("agua") || pokemonDef.equals("fuego") || pokemonDef.equals("planta") || pokemonDef.equals("electrico")){
+            switch (pokemonAtaq){
+                case "agua":
+                    if(pokemonDef.equals("agua")){
+                        efectividad = 1;
+                    } else if (pokemonDef.equals("fuego")) {
+                        efectividad = 0.5;
+                    } else if (pokemonDef.equals("planta") || pokemonDef.equals("electrico")) {
+                        efectividad = 2;
+                    }
+                break;
 
-    public void cal(String pokemonAtaq, int atq, String pokemonDef, int def){
-        switch (pokemonAtaq){
-            case "Agua":
+                case "fuego":
+                    if(pokemonDef.equals("agua")){
+                        efectividad = 2;
+                    } else if (pokemonDef.equals("fuego") || pokemonDef.equals("planta")) {
+                        efectividad = 0.5;
+                    } else if (pokemonDef.equals("electrico")) {
+                        efectividad = 1;
+                    }
+                break;
 
-            break;
+                case "planta":
+                    if(pokemonDef.equals("agua") || pokemonDef.equals("fuego")){
+                        efectividad = 2;
+                    } else if (pokemonDef.equals("planta")) {
+                        efectividad = 0.5;
+                    } else if (pokemonDef.equals("electrico")) {
+                        efectividad = 1;
+                    }
+                break;
 
-            case "Fuego":
+                case "electrico":
+                    if(pokemonDef.equals("agua") || pokemonDef.equals("fuego")){
+                        efectividad = 1;
+                    } else if (pokemonDef.equals("planta") || pokemonDef.equals("electrico")) {
+                        efectividad = 0.5;
+                    }
+                break;
 
-            break;
-
-            case "Planta":
-
-            break;
-
-            case "Electrico":
-
-            break;
-
-            default:
-                System.out.println("Eliga un pokemon valido.");
-            break;
+                default:
+                    System.err.println("Eliga un Pókemon de ataque valido.");
+                return;
+            }
+        }else{
+            System.err.println("Eliga un Pókemon de defensa valido.");
+            return;
         }
-    }
 
+        danio = 50*(atq/def)*efectividad;
+        String tpEfe = efectividad == 2 ? "Súper Efectivo" : efectividad == 1 ? "Neutral" : "No es muy efectivo";
+        System.out.println("El daño causado durante la batalla fue de: "+danio);
+        System.out.println("El ataque tuvo una efectividad del x"+efectividad +" ("+tpEfe+")");
+    }
 }
--- src/main/java/org/nkl/Main.java
+++ src/main/java/org/nkl/Main.java
@@ -1,36 +1,48 @@
 package org.nkl;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Pokemon> pk = new ArrayList<>();
+        boolean continuar = true;
 
         String pokemonAtq, pokemonDef;
         int pkAtq, pkDef;
 
-        System.out.println("---- La batalla Pókemon ----\n");
+        System.out.println("- - - - La batalla Pókemon - - - -");
+        System.out.println("Bienvenid@ a la batalla, eliga los Pókemones...");
 
-        System.out.println("Pokémon de Ataque: ");
-        pokemonAtq = sc.next();
-        System.out.println("Ingrese el Ataque: ");
-        pkAtq = sc.nextInt();
+        do{
+            System.out.println("Pókemones Disponibles:  \n⭒ Agua \n⭒ Fuego " +
+                    "\n⭒ Planta \n⭒ Eléctrico \n- - - - - - - - - - - - - - - - \n");
+            try{
+                System.out.println("Pokémon de Ataque: ");
+                pokemonAtq = sc.next();
+                System.out.println("Ingrese el Ataque: (1-100)");
+                pkAtq = sc.nextInt();
 
-        pk.add(new Pokemon(pokemonAtq, "atq", pkAtq));
+                System.out.println("Pokémon de Defensa: ");
+                pokemonDef = sc.next();
+                System.out.println("Ingrese la Defensa: (1-100)");
+                pkDef = sc.nextInt();
 
-        System.out.println("Pokémon de Defensa: ");
-        pokemonDef = sc.next();
-        System.out.println("Ingrese la Defensa: ");
-        pkDef = sc.nextInt();
+                if((pkAtq > 0 && pkAtq <= 100) && (pkDef > 0 && pkDef < 100)){
+                    CalcDan cl = new CalcDan();
+                    cl.cal(pokemonAtq.toLowerCase(), pkAtq, pokemonDef.toLowerCase(), pkDef);
+                }else{
+                    System.err.println("El ataque y la defensa deben estar dentro del rango permitido (1-100)");
+                }
+            }catch (InputMismatchException e){
+                System.err.println("El ataque y la defensa deben ser números.");
+            }
 
-        pk.add(new Pokemon(pokemonDef, "def", pkDef));
-
-
-//        System.out.println("Pokemon de defensa");
-
+            System.out.println("\n- - - - - Menú - - - - -" +
+                    "\n1. Iniciar batalla nueva " +
+                    "\nOtra tecla para salir" +
+                    "\n- - - - - - - - - - - - -");
+            continuar = sc.next().equals("1");
+        }while(continuar);
     }
 }
